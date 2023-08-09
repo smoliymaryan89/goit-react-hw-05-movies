f3a688f332b0edfcf7cc89abe454ac2d75fe0d48
smoliymaryan89 <112328727+smoliymaryan89@users.noreply.github.com>
2023-08-09 16:00:24 +0000

    Deploying to gh-pages from @ smoliymaryan89/goit-react-hw-05-movies@56afc32c41318c0e21cfb7ea3592288f2909745d 🚀

---
+++ static/js/520.11321796.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[520],{4520:function(e,n,i){i.r(n);i(2791);var s=i(184);n.default=function(){return(0,s.jsxs)("div",{children:[(0,s.jsx)("h1",{children:"404"}),(0,s.jsx)("p",{children:"Page not found..."})]})}}}]);
+//# sourceMappingURL=520.11321796.chunk.js.map
